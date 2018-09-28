7748da9eefe8e33e2fce1a1e455f8a7ea4f3b541
Evgen Kolenkov <kolenkoff@icloud.com>
2018-09-28 13:59:45 +0300

    first commit

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Evgeniy Kolenkov on 03.05.18.
+//  Copyright © 2018 Evgeniy Kolenkov. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+}
---
+++ FlickrSearch/FlickrPhotoHeaderView.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoHeaderView.swift
+//  FlickrSearch
+//
+//  Created by Evgeniy Kolenkov on 9/28/18.
+//  Copyright © 2018 Richard Turton. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoHeaderView: UICollectionReusableView {
+        
+}
